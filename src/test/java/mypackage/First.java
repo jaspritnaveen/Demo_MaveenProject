4f95905bf2fff06f37a2248db8070e2115a5e588
jaspritnaveen <naveenkumar.cs21@bitsathy.ac.in>
2024-07-16 05:25:07 +0530

    Add existing project files

---
+++ src/test/java/mypackage/First.java
@@ -0,0 +1,16 @@
+package mypackage;
+import org.testng.annotations.Test;
+
+
+ 
+
+	public class First {
+		// TODO Auto-generated method stub
+		@Test
+		public void get() {
+			System.out.println("hey");
+			
+		}
+	}
+
+
